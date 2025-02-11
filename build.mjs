5e910a27aac3eec31cf848cf45f4415eb414ff0d
Jared Hoover <98363870+jhadobe@users.noreply.github.com>
2025-02-11 17:29:53 -0600

    Initial commit

---
+++ build.mjs
@@ -0,0 +1,10 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { overrideGQLOperations } from '@dropins/build-tools/gql-extend.js';
+
+overrideGQLOperations([
+  // ACCS does not have Downloadable Items
+  {
+    npm: '@dropins/storefront-cart',
+    skipFragments: ['DOWNLOADABLE_CART_ITEMS_FRAGMENT'],
+  },
+]);
